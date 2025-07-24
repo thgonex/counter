da7fd75ed7582367efcd30ef7567b00118b78429
Denys Khorlikov <denyskhorlikov@gmail.com>
2025-07-24 11:45:09 +0300

    Add save in localStorage

--- count.js
+++ count.js
@@ -3,19 +3,28 @@ const addBtn = document.querySelector("#add")
 const reduceBtn = document.querySelector("#reduce")
 const resetBtn = document.querySelector("#reset")
 
-let counter = 0 
+let counter = Number(localStorage.getItem("counter"))
+
+if (isNaN(counter)) {
+  counter = 0
+}
+
+numberText.textContent = counter
 
 addBtn.addEventListener("click", () => {
     counter++
     numberText.textContent = counter
+    localStorage.setItem("counter", counter)
 })
 
 reduceBtn.addEventListener("click", () => {
     counter--
     numberText.textContent = counter
+    localStorage.setItem("counter", counter)
 })
 
 resetBtn.addEventListener("click", () => {
     counter = 0
     numberText.textContent = counter
+    localStorage.setItem("counter", counter)
 })
